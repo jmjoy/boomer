7177e1eba1dbe7e79b1c81a6b7a26a8a7b8d199a
jmjoy <918734043@qq.com>
2015-12-30 00:18:19 +0800

    already fork.

---
+++ boomer.go
@@ -0,0 +1,190 @@
+// Package boomer provides commands to run load tests and display results.
+package boomer
+
+import (
+	"crypto/tls"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/rakyll/pb"
+)
+
+type result struct {
+	err           error
+	statusCode    int
+	duration      time.Duration
+	contentLength int64
+}
+
+type Boomer struct {
+	// Request is the request to be made.
+	Request *http.Request
+
+	RequestBody string
+
+	// N is the total number of requests to make.
+	N int
+
+	// C is the concurrency level, the number of concurrent workers to run.
+	C int
+
+	// Timeout in seconds.
+	Timeout int
+
+	// Qps is the rate limit.
+	Qps int
+
+	// AllowInsecure is an option to allow insecure TLS/SSL certificates.
+	AllowInsecure bool
+
+	// DisableCompression is an option to disable compression in response
+	DisableCompression bool
+
+	// DisableKeepAlives is an option to prevents re-use of TCP connections between different HTTP requests
+	DisableKeepAlives bool
+
+	// Output represents the output type. If "csv" is provided, the
+	// output will be dumped as a csv stream.
+	Output string
+
+	// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
+	// Optional.
+	ProxyAddr *url.URL
+
+	// ReadAll determines whether the body of the response needs
+	// to be fully consumed.
+	ReadAll bool
+
+	bar     *pb.ProgressBar
+	results chan *result
+}
+
+func (b *Boomer) startProgress() {
+	if b.Output != "" {
+		return
+	}
+	b.bar = pb.New(b.N)
+	b.bar.Format("Bom !")
+	b.bar.Start()
+}
+
+func (b *Boomer) finalizeProgress() {
+	if b.Output != "" {
+		return
+	}
+	b.bar.Finish()
+}
+
+func (b *Boomer) incProgress() {
+	if b.Output != "" {
+		return
+	}
+	b.bar.Increment()
+}
+
+// Run makes all the requests, prints the summary. It blocks until
+// all work is done.
+func (b *Boomer) Run() {
+	b.results = make(chan *result, b.N)
+	b.startProgress()
+
+	start := time.Now()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		<-c
+		// TODO(jbd): Progress bar should not be finalized.
+		newReport(b.N, b.results, b.Output, time.Now().Sub(start)).finalize()
+	}()
+
+	b.runWorkers()
+	b.finalizeProgress()
+	newReport(b.N, b.results, b.Output, time.Now().Sub(start)).finalize()
+	close(b.results)
+}
+
+func (b *Boomer) runWorker(wg *sync.WaitGroup, ch chan *http.Request) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: b.AllowInsecure,
+		},
+		DisableCompression: b.DisableCompression,
+		DisableKeepAlives:  b.DisableKeepAlives,
+		// TODO(jbd): Add dial timeout.
+		TLSHandshakeTimeout: time.Duration(b.Timeout) * time.Millisecond,
+		Proxy:               http.ProxyURL(b.ProxyAddr),
+	}
+	client := &http.Client{Transport: tr}
+	for req := range ch {
+		s := time.Now()
+
+		var code int
+		var size int64
+
+		resp, err := client.Do(req)
+		if err == nil {
+			size = resp.ContentLength
+			code = resp.StatusCode
+			if b.ReadAll {
+				_, err = io.Copy(ioutil.Discard, resp.Body)
+			}
+			resp.Body.Close()
+		}
+
+		wg.Done()
+		b.incProgress()
+		b.results <- &result{
+			statusCode:    code,
+			duration:      time.Now().Sub(s),
+			err:           err,
+			contentLength: size,
+		}
+	}
+}
+
+func (b *Boomer) runWorkers() {
+	var wg sync.WaitGroup
+	wg.Add(b.N)
+
+	var throttle <-chan time.Time
+	if b.Qps > 0 {
+		throttle = time.Tick(time.Duration(1e6/(b.Qps)) * time.Microsecond)
+	}
+
+	jobsch := make(chan *http.Request, b.N)
+	for i := 0; i < b.C; i++ {
+		go b.runWorker(&wg, jobsch)
+	}
+
+	for i := 0; i < b.N; i++ {
+		if b.Qps > 0 {
+			<-throttle
+		}
+		jobsch <- cloneRequest(b.Request, b.RequestBody)
+	}
+	close(jobsch)
+	wg.Wait()
+}
+
+// cloneRequest returns a clone of the provided *http.Request.
+// The clone is a shallow copy of the struct and its Header map.
+func cloneRequest(r *http.Request, body string) *http.Request {
+	// shallow copy of the struct
+	r2 := new(http.Request)
+	*r2 = *r
+	// deep copy of the Header
+	r2.Header = make(http.Header, len(r.Header))
+	for k, s := range r.Header {
+		r2.Header[k] = append([]string(nil), s...)
+	}
+	r2.Body = ioutil.NopCloser(strings.NewReader(body))
+	return r2
+}
